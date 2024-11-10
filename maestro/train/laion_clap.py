9d0e9292f632c19cbea13c6663b2eb0496920cc2
ken <ken@kenmyers.io>
2024-11-09 23:13:56 -0600

    integrate eval intop trainloop

--- maestro/train/laion_clap.py
+++ maestro/train/laion_clap.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import shutil
 from pathlib import Path
 import os
+from maestro.eval.gtzan import eval_laion_clap
 
 def gtzan_collate(batch):
     return {
@@ -11,8 +12,14 @@ def gtzan_collate(batch):
         'features': torch.stack([item['features'] for item in batch])
     }
 
-def train_gtzan_model(*, output_dir, train_loader, val_loader, model, epochs, device, optimizer, scheduler, label_ids,
-                      disable_tokenizers_parallelism = True, clear_output_dir = False, ckpt_interval = None):
+def train_gtzan_model(*, output_dir, train_loader, val_loader, model, epochs, device, optimizer, scheduler, label_ids, label_attn_masks,
+                      disable_tokenizers_parallelism = True, clear_output_dir = False, ckpt_interval = None, top_k=None):
+
+    
+    if top_k is not None:
+        scalar_k = not isinstance(top_k, list)
+        if not scalar_k:
+            top_k = sorted(top_k)
 
     if disable_tokenizers_parallelism:
         print("Disabling tokenizers parallelism")
@@ -27,6 +34,7 @@ def train_gtzan_model(*, output_dir, train_loader, val_loader, model, epochs, de
 
     model.to(device)
     label_ids = label_ids.to(device)
+    label_attn_masks = label_attn_masks.to(device)
     
     criterion = nn.CrossEntropyLoss()
 
@@ -34,10 +42,10 @@ def train_gtzan_model(*, output_dir, train_loader, val_loader, model, epochs, de
     val_losses = []
 
     for epoch in range(epochs):
-        # Training
+        # ----- Training -----
         model.train()
         train_loss = 0
-        for batch in tqdm(train_loader, desc=f"Epoch {epoch} - Training"):
+        for batch in tqdm(train_loader, desc=f"Epoch {epoch+1} - Training"):
             
             # Zero the gradients
             optimizer.zero_grad()
@@ -47,7 +55,7 @@ def train_gtzan_model(*, output_dir, train_loader, val_loader, model, epochs, de
             features = batch['features'].to(device)
 
             # Generate label embeddings
-            label_embeddings = model.get_text_features(label_ids)
+            label_embeddings = model.get_text_features(label_ids, attention_mask=label_attn_masks)
 
             # Generate audio embeddings
             audio_embeddings = model.get_audio_features(features)
@@ -63,22 +71,27 @@ def train_gtzan_model(*, output_dir, train_loader, val_loader, model, epochs, de
             loss.backward()
             optimizer.step()
         
-        print(f"Epoch {epoch} - Training loss: {train_loss / len(train_loader)}")
+        # Calc loss
+        avg_train_loss = train_loss / len(train_loader)
+        train_losses.append(avg_train_loss)
+        print(f"Epoch {epoch + 1} - Training loss: {avg_train_loss}")
 
-        # Validation
+        # ----- Validation -----
         model.eval()
         val_loss = 0
+        
+        # Only need to compute label embeddings once since we're not training
+        label_embeddings = model.get_text_features(label_ids, attention_mask=label_attn_masks)
 
         with torch.no_grad():
-            for batch in tqdm(val_loader, desc=f"Epoch {epoch} - Validation"):
+            for batch in tqdm(val_loader, desc=f"Epoch {epoch+1} - Validation"):
                 
                 # Move data to device
                 labels = batch['label'].to(device)
                 features = batch['features'].to(device)
 
                 # Generate embeddings
-                label_embeddings = model.get_text_features(label_ids)
-                audio_embeddings = model.get_audio(features)
+                audio_embeddings = model.get_audio_features(features)
 
                 # Sim matrix
                 logits = torch.matmul(audio_embeddings, label_embeddings.T)
@@ -86,20 +99,31 @@ def train_gtzan_model(*, output_dir, train_loader, val_loader, model, epochs, de
                 # Compute loss
                 loss = criterion(logits, labels)
                 val_loss += loss.item()
+        
+        #Calc val loss
+        avg_val_loss = val_loss / len(val_loader)
+        val_losses.append(avg_val_loss)
+        print(f"Epoch {epoch + 1} - Validation loss: {avg_val_loss}")
 
-        print(f"Epoch {epoch} - Validation loss: {val_loss / len(val_loader)}")
+        # Calc accuracy
+        if top_k is not None:
+            accuracy = eval_laion_clap(model, val_loader=val_loader, label_embeddings=label_embeddings, top=top_k)
 
-        val_loss = val_loss / len(val_loader)
-        train_loss = train_loss / len(train_loader)
+            if scalar_k:
+                print(f"Epoch {epoch + 1} - Accuracy: {accuracy}")
+            else:
+                acc_msg = f"Epoch {epoch+1} - Accuracy:"
+                for k, acc in zip(top_k, accuracy):
+                    acc_msg += f" Top {k}: {acc}"
+                print(acc_msg)
 
-        train_losses.append(train_loss)
-        val_losses.append(val_loss)
-
+        # Step the scheduler
         scheduler.step(val_loss)
 
+        # Save checkpoint
         if ckpt_interval is not None and epoch % ckpt_interval == 0:
             torch.save(model.state_dict(), output_dir / f"model_{epoch}.pt")
-
+        
         #TODO: graphs, accuracy
     
     torch.save(model.state_dict(), output_dir / f"model_final.pt")
