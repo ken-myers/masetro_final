e10a4c658b99d5de4d38d9ce90a9fbcd72c9bf86
ken <ken@kenmyers.io>
2024-11-11 16:10:23 -0600

    add aug preprocessing commands

--- maestro/preprocessing/__main__.py
+++ maestro/preprocessing/__main__.py
@@ -3,8 +3,22 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
 
-    sub_parsers = parser.add_subparsers(dest='model')
+    sub_parsers = parser.add_subparsers(dest='mode')
     sub_parsers.required = True
+
+
+    # ========== Augmentation ============
+    # Augmentation command
+    augmentation_parser = sub_parsers.add_parser('augment')
+    
+    augmentation_parser.add_argument('data_path', type=str, help='Path to the directory containing the wav files')
+    augmentation_parser.add_argument('--sample_rate', type=int, default=48000, help='Sample rate of the output audio files')
+    augmentation_parser.add_argument('--length', type=int, default=None, help='Number of augmented files to generate')
+    augmentation_parser.add_argument('--aug_factor', type=int, default=None, help='Number of augmented files to generate per input file')
+    augmentation_parser.add_argument('--num_workers', type=int, default=1, help='Number of workers to use for augmentation')
+
+
+    # ========== Laion clap preprocessing ============
 
     # Preprocessing command
     laion_clap_parser = sub_parsers.add_parser('laion_clap')
@@ -13,11 +27,8 @@ def main():
     laion_clap_parser.add_argument('--num_workers', type=int, default=1, help='Number of workers to use for preprocessing')
     laion_clap_parser.add_argument('--resample_only', action='store_true', help='Only resample the audio files')
 
-
-
     laion_clap_subparsers = laion_clap_parser.add_subparsers(dest='data_origin')
 
-    # ========== Laion clap preprocessing ============
 
     #synth
     laion_clap_synth_parser = laion_clap_subparsers.add_parser('synth')    
@@ -35,7 +46,7 @@ def main():
 
     args = parser.parse_args()
 
-    if args.model == 'laion_clap':
+    if args.mode == 'laion_clap':
         if args.data_origin == 'synth':
             from maestro.preprocessing.laion_clap import preprocess_synth
             preprocess_synth(data_path=args.data_path, out_path=args.out_path, target_sample_rate=args.target_sample_rate, num_workers=args.num_workers, resample_only=args.resample_only)
@@ -44,6 +55,10 @@ def main():
             preprocess_gtzan(input_dir=args.input_dir, output_dir=args.output_dir, target_sample_rate=args.target_sample_rate, num_workers=args.num_workers, resample_only=args.resample_only)
         else:
             raise ValueError(f"Unknown data origin: {args.data_origin}")
+    elif args.mode == 'augment':
+        from maestro.preprocessing.augmentation import augment_audio
+        augment_audio(data_path=args.data_path, sample_rate=args.sample_rate, length=args.length, aug_factor=args.aug_factor)
+        #TODO: validate args
     else:
         raise ValueError(f"Unknown model: {args.model}")
 
